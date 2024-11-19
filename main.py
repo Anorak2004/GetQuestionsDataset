e693aafffbc573ab9ee5e4da305cf34c1e93ef33
Anorak2004 <Anorak2004@outlook.com>
2024-11-19 14:49:46 +0800

    v0.3.3

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from controller import *
 
 def main():
     # 填写用户名和密码
-    username = 'Anorak'  # 替换为你的用户名
-    password = '123456'  # 替换为你的密码
+    username = 'test'  # 替换为你的用户名
+    password = '135246'  # 替换为你的密码
 
     # 创建登录实例并执行登录
     login = Login(username, password)
@@ -28,7 +28,7 @@ def main():
     if login_response.status_code == 200:
         controller = Controller(login)
         
-        for i in range(157,171):
+        for i in range(171,201):
             controller.process_loop(i)
 
 if __name__ == "__main__":
